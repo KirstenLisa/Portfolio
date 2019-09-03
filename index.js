9349266bfb12ed9582e27741381a293fab9dec39
Kirsten Kustusch <kirsten.kustusch@gmail.com>
2019-09-03 22:15:18 +0300

    first version finished

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-/* $('html, body').animate({
-    scrollTop: $('footer').offset().top
-    //scrollTop: $('#your-id').offset().top
-    //scrollTop: $('.your-class').offset().top
- }, 'slow'); */
+
+
+function startPage() {
+  $('#portfolio').hide();
+  $('#aboutText2').hide();
+  $('.name').hide();
+  $('.name').fadeIn(2000);
+  $('#portfolioButton').click(function() {
+      showPortfolio();
+    })
+  $('.portfolioLink').on('click', function() {
+    console.log("portfolio nav")
+    showPortfolio();
+  })
+  $('.aboutLink').on('click', function() {
+    console.log("about nav");
+    showAbout();
+  })
+}
+startPage();
+
+
+function showPortfolio() {
+  $('#portfolio').fadeIn();
+  $('html, body').animate({ scrollTop: $("#portfolio").offset().top - (150)}, 1000);
+  $('.portfolioLink').addClass('navLinkOpen');
+  $('.aboutLink').removeClass('navLinkOpen');
+  $('#portfolioButton').hide();
+} 
+
+function showAbout() {
+  $('#aboutText2').fadeIn();
+  $('#aboutHeadline').fadeIn();
+  $('html, body').animate({ scrollTop: $("#aboutMe").offset().top - (70)  }, 500);
+  $('.aboutLink').addClass('navLinkOpen');
+  $('.portfolioLink').removeClass('navLinkOpen');
+}
+
+
